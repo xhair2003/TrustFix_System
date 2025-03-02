8fa5a936577dbbe93002a9b426341af11172cea8
nvh01022003 <nvh01022003@gmail.com>
2025-03-02 11:32:44 +0700

    merge FE

---
+++ client/src/pages/users/HomePage/HomePage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './HomePage.css';
+import introductionImage from '../../../assets/Images/introduction.jpg';
+import SearchBar from '../../../component/users/SearchBar/SearchBar';
+
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            <div className="introduction-section">
+                <img src={introductionImage} alt="Introduction" className="introduction-image" />
+            </div>
+
+            <div className="introduction-text">
+                <h2>Cách tốt hơn để tìm thợ sửa chữa của bạn</h2>
+                <p>
+                    Cách tốt hơn để tìm thợ sửa chữa của bạn. Cách tốt hơn để tìm thợ sửa chữa của bạn, Cách tốt hơn để tìm thợ sửa chữa của bạn Cách tốt hơn
+                </p>
+            </div>
+
+            <div className="search-section">
+                <SearchBar />
+            </div>
+
+            <div className="content-section">
+                {/* Nội dung khác sẽ được thêm vào đây */}
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ client/src/pages/users/MasterLayoutUser/MasterLayoutUser.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './MasterLayoutUser.css';
+import userImage from '../../../assets/Images/user.jpg';
+import { FaUser, FaLock, FaHistory, FaExclamationCircle, FaStar, FaSignOutAlt, FaArrowLeft } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import logo from "../../../assets/Images/logo.png";
+import onlyLogo from "../../../assets/Images/onlyLogo.jpg";
+
+const MasterLayoutUser = ({ children }) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const navigate = useNavigate();
+
+    const toggleSidebar = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    const handleItemClick = (item) => {
+        setSelectedItem(item.name);
+        navigate(item.path);
+    };
+
+    return (
+        <div className="master-layout">
+            <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+                <div className='upper-sidebar'>
+                    <button className="back-button" onClick={() => navigate("/")}>
+                        <FaArrowLeft /> {/* Biểu tượng quay về */}
+                    </button>
+                    <div className="user-info">
+                        <img src={userImage} alt="User" className="user-image" />
+                        {!isCollapsed && (
+                            <>
+                                <h2>Nguyễn Văn Hoành</h2>
+                                <p>nvh01022003g@gmail.com</p>
+                            </>
+                        )}
+                    </div>
+
+                    <button className="toggle-button" onClick={toggleSidebar}>
+                        {isCollapsed ? '☰' : '✖'}
+                    </button>
+                </div>
+
+                <div className="menu-items">
+                    <div className={`menu-item ${selectedItem === 'profile' ? 'active' : ''}`} onClick={() => handleItemClick({ path: "/profile", name: 'profile' })}>
+                        {isCollapsed ? <FaUser /> : <> <FaUser /> Thông tin cá nhân</>}
+                    </div>
+                    <div className={`menu-item ${selectedItem === 'change-password' ? 'active' : ''}`} onClick={() => handleItemClick({ path: "/change-password", name: 'change-password' })}>
+                        {isCollapsed ? <FaLock /> : <> <FaLock /> Đổi mật khẩu</>}
+                    </div>
+                    <div className={`menu-item ${selectedItem === 'view-repair-booking-history' ? 'active' : ''}`} onClick={() => handleItemClick({ path: "/view-repair-booking-history", name: 'view-repair-booking-history' })}>
+                        {isCollapsed ? <FaHistory /> : <> <FaHistory /> Lịch sử đặt dịch vụ</>}
+                    </div>
+                    <div className={`menu-item ${selectedItem === 'complain-repair-man' ? 'active' : ''}`} onClick={() => handleItemClick({ path: "/complain-repair-man", name: 'complain-repair-man' })}>
+                        {isCollapsed ? <FaExclamationCircle /> : <> <FaExclamationCircle /> Khiếu nại</>}
+                    </div>
+                    <div className={`menu-item ${selectedItem === 'upgrade-repair-man' ? 'active' : ''}`} onClick={() => handleItemClick({ path: "/upgrade-repair-man", name: 'upgrade-repair-man' })}>
+                        {isCollapsed ? <FaStar /> : <> <FaStar /> Nâng cấp lên thợ</>}
+                    </div>
+                    <div className={`menu-item ${selectedItem === 'logout' ? 'active' : ''}`} onClick={() => {/* Handle logout */ }}>
+                        {isCollapsed ? <FaSignOutAlt /> : <> <FaSignOutAlt /> Đăng xuất</>}
+                    </div>
+                </div>
+
+                <div>
+                    <img src={isCollapsed ? onlyLogo : logo} alt="Logo" className="lower-sidebar" />
+                </div>
+
+            </div>
+            <div className="content">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default MasterLayoutUser;
