e20c3049f386af21e8965b13c3b1e3265416098e
lilducanh <ducanh8903@gmail.com>
2025-03-11 09:59:05 +0700

    Update

--- server/src/routes/CustomerRoutes.js
+++ server/src/routes/CustomerRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AuthMiddleware = require("../middlewares/authMiddleware");
 const CustomerController = require("../controllers/CustomerController");
 const upload = require("../middlewares/upload_IMG");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 
 router.get(
@@ -17,4 +18,11 @@ router.post(
   CustomerController.createComplaint
 );
 
+router.post("/manage-infor",authMiddleware.verifyToken,upload.single("image"),CustomerController.updateInformation);
+
+//rating
+// router.get("/rating-by-id/:id",CustomerController.getRatingById);
+router.post("/rating",authMiddleware.verifyToken,CustomerController.addRating);
+router.put("/edit-rating",authMiddleware.verifyToken,CustomerController.editRating);
+router.delete("/delete-rating/:id",authMiddleware.verifyToken,CustomerController.deleteRating);
 module.exports = router;
