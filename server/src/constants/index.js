f07bfc6e781e5e033cece85190a65727eccae4c4
thaisduong <duong482003@gmail.com>
2025-03-27 21:51:18 +0700

    fix confict

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-import nodemailer from 'nodemailer'; // Import nodemailer để gửi email
-
-export const MONTHLY_FEE = 100000; // 100,000 VND per month
-
-export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
-
-export const sendEmail = async (to, subject, htmlContent) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER, 
-            pass: process.env.EMAIL_PASS 
-        }
-    });
-
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        html: htmlContent
-    };
-
-    await transporter.sendMail(mailOptions);
-};
-=======
 const MONTHLY_FEE = 100000;
 
 const CRON_TIME = {
@@ -42,4 +10,3 @@ const CRON_TIME = {
 };
 
 module.exports = { MONTHLY_FEE, CRON_TIME};
->>>>>>> Stashed changes
