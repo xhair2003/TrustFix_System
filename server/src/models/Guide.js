c47381472efb4e0864a8419c45540f1a76f73978
lilducanh <ducanh8903@gmail.com>
2025-04-18 16:26:35 +0700

    Fix API guide

--- server/src/models/Guide.js
+++ server/src/models/Guide.js
@@ -8,11 +8,10 @@ const mongoose = require('mongoose');
      },
      title: { type: String, required: true },
      type: { type: String, enum: ['video', 'article', 'images'], required: true }, // Thêm 'images'
-     content: { type: String, required: true },
+     content: { type: [String], required: true },
      description: { type: String },
      category: { type: String },
      tags: { type: mongoose.Schema.Types.Mixed }, // JSON or array
-     created_by: { type: Number, required: true },
      
  }, {
      timestamps: true,
